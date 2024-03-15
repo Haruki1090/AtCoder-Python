4ddd8e4580691751c1af6ec3407254f86aa4866c
Haruki1090 <haruharu.com.109607@gmail.com>
2024-03-15 17:00:33 +0900

    4問目

---
+++ ABC081B - Shift only.py
@@ -0,0 +1,25 @@
+# 問題文
+# 黒板に N 個の正の整数 A_1 ,...,A_Nが書かれています
+
+# すぬけ君は，黒板に書かれている整数がすべて偶数であるとき，次の操作を行うことができます．
+
+# 黒板に書かれている整数すべてを 2 で割ったものに置き換える．
+# すぬけ君は最大で何回操作を行うことができるかを求めてください．
+
+# 制約
+# 1 ≤ N ≤ 200
+# 1 ≤ A_i ≤ 10^9
+
+N = int(input()) # N個の整数
+A = list(map(int, input().split())) # N個の整数をリストに格納
+
+for i in range(N):
+    count = 0
+    while A[i] % 2 == 0:
+        A[i] = A[i] / 2
+        count += 1
+    if i == 0:
+        min_count = count
+    elif count < min_count:
+        min_count = count
+print(min_count)
